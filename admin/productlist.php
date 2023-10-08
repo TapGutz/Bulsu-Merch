735cc13156240a77148a2cc3d91b69044a391694
Raymund Lapuz <raymund.lapuz.x@bulsu.edu.ph>
2023-10-08 21:46:10 +0800

    fix delete on productlist

--- admin/productlist.php
+++ admin/productlist.php
@@ -4,32 +4,33 @@ include("../db.php");
 include "sidenav.php";
 include "topheader.php";
 ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-        
-        
+ <!-- End Navbar -->
+ <div class="content">
+     <div class="container-fluid">
+
+
          <div class="col-md-14">
-            <div class="card ">
-              <div class="card-header card-header-primary">
-                <h4 class="card-title"> Products List</h4>
-                
-              </div>
-              <div class="card-body">
-                <div class="col-md-2 offset-md-10"> <a class=" btn btn-primary " href="index.php?page=addproduct">Add New</a></div> 
-                <br>
-                <div class="table-responsive ps">
-                  <table class="table table-striped " id="prod">
-                    <thead class="">
-                      <tr>
-                        <th>Image</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <?php 
+             <div class="card ">
+                 <div class="card-header card-header-primary">
+                     <h4 class="card-title"> Products List</h4>
+
+                 </div>
+                 <div class="card-body">
+                     <div class="col-md-2 offset-md-10"> <a class=" btn btn-primary "
+                             href="index.php?page=addproduct">Add New</a></div>
+                     <br>
+                     <div class="table-responsive ps">
+                         <table class="table table-striped " id="prod">
+                             <thead class="">
+                                 <tr>
+                                     <th>Image</th>
+                                     <th>Name</th>
+                                     <th>Price</th>
+                                     <th>Action</th>
+                                 </tr>
+                             </thead>
+                             <tbody>
+                                 <?php 
 
                         $result=mysqli_query($con,"SELECT product_id,product_image, product_title,product_price from products    order by product_title asc");
 
@@ -39,25 +40,51 @@ include "topheader.php";
                         <td>$price</td>
                         <td class='text-center'>
                         <a class=' btn btn-primary btn-sm' href='index.php?page=addproduct&id=$product_id'>Edit</a>
-                        <a class=' btn btn-danger btn-sm' href='clothes_list.php?product_id=$product_id&action=delete'>Delete</a>
+                        <a class=' btn btn-danger btn-sm clothes_list' href='javascript:void(0)' data-id ='$product_id'>Delete</a>
+                        
                         </td></tr>";
                         }
 
                         ?>
-                    </tbody>
-                  </table>
-                <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
-              </div>
-            </div>
-            
-           
+                             </tbody>
+                         </table>
+                         <div class="ps__rail-x" style="left: 0px; bottom: 0px;">
+                             <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+                         </div>
+                         <div class="ps__rail-y" style="top: 0px; right: 0px;">
+                             <div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
 
-          </div>
-          
-          
-        </div>
-      </div>
 
-      <script>
-        $('#prod').dataTable()
-      </script>
+
+         </div>
+
+
+     </div>
+ </div>
+
+ <script>
+$('#prod').dataTable()
+
+$('.clothes_list').click(function() {
+
+    var conf = confirm('Are you sure to delete this data?');
+    if (conf == true) {
+        $.ajax({
+            url: 'clothes_list.php?id=' + $(this).attr('data-id'),
+            success: function(resp) {
+                if (resp == 1) {
+                    alert('Data succefully deleted.')
+                    location.reload()
+
+                }
+            }
+        })
+    }
+
+
+})
+ </script>
